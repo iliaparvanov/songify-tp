8fedc5b44679d64d678fbe40c26d4f07e44bb582
iliapa1 <ilia.nikolaev.parvanov@gmail.com>
2019-06-15 00:57:37 +0300

    Added foundation of Rails rest api

---
+++ songify_api/app/controllers/artists_controller.rb
@@ -0,0 +1,43 @@
+class ArtistsController < ApplicationController
+    before_action :set_artist, only: [:show, :update, :destroy]
+
+  # GET /todos
+  def index
+    @artists = Artist.all
+    json_response(@artists)
+  end
+
+  # POST /artists
+  def create
+    @artist = Artist.create!(artist_params)
+    json_response(@artist, :created)
+  end
+
+  # GET /artist/:id
+  def show
+    json_response(@artist)
+  end
+
+  # PUT /artists/:id
+  def update
+    @artist.update(artist_params)
+    head :no_content
+  end
+
+  # DELETE /artists/:id
+  def destroy
+    @artist.destroy
+    head :no_content
+  end
+
+  private
+
+  def artist_params
+    # whitelist params
+    params.permit(:name)
+  end
+
+  def set_artist
+    @artist = Artist.find(params[:id])
+  end
+end
---
+++ songify_api/app/models/artist.rb
@@ -0,0 +1,5 @@
+class Artist < ApplicationRecord
+    has_many :albums, dependent: :destroy
+
+    validates_presence_of :name
+end
