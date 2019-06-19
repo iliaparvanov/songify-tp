abc6133dcfdc85d39b79c57eb0680314e11d1cec
Ilia Parvanov <ilia.nikolaev.parvanov@gmail.com>
2019-06-19 09:54:57 +0300

    @album is now a real object (hopefully)

--- songify_api/app/controllers/albums_controller.rb
+++ songify_api/app/controllers/albums_controller.rb
@@ -14,7 +14,7 @@ class AlbumsController < ApplicationController
 
     # POST /artists/:artist_id/albums
     def create
-        @artist.albums.create!(album_params)
+        @album = @artist.albums.create!(album_params)
         json_response(@album, :created)
     end
 
