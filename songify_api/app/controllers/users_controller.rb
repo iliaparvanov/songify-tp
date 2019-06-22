656a8b8be174db81a4e7411baa4faeaa86a85b3b
Gdemirev01 <gdemirev78@gmail.com>
2019-06-22 13:12:23 +0300

    user uniqueness and signup security key

--- songify_api/app/controllers/users_controller.rb
+++ songify_api/app/controllers/users_controller.rb
@@ -1,10 +1,21 @@
 class UsersController < ApplicationController
   skip_before_action :authorize_request, only: :create
   def create
-    user = User.create!(user_params)
-    auth_token = AuthenticateUser.new(user.email, user.password).call
-    response = { message: Message.account_created, auth_token: auth_token }
-    json_response(response, :created)
+    if "secret_key_that_no_one_knows" == params["key_for_signup"]
+
+      if User.find_by(email: params["email"]).nil?
+        user = User.create!(user_params)
+        auth_token = AuthenticateUser.new(user.email, user.password).call
+        response = { message: Message.account_created, auth_token: auth_token }
+        json_response(response, :created)
+      else
+        response = { message: "User with that email exists"}
+        json_response(response)
+      end
+    else
+      response = { message: "Invalid key"}
+      json_response(response)
+    end
 
   end
 
